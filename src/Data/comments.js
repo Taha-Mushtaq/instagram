2b4550fbd6fc4b19aab6742f97bdff8e9d3a20bc
mobeenaslam007 <mobeenaslam173@gmail.com>
2022-12-08 20:21:07 +0500

    comment submit

--- src/Data/comments.js
+++ src/Data/comments.js
@@ -1,23 +1,23 @@
-export const commentsData=[
-    {
-        id:'1',
-        postId:'1',
-        userId:'1',
-        comment:'SHukriya',
-        time: '23min'
-    },
-    {
-        id:'2',
-        postId:'1',
-        userId:'2',
-        comment:'Great',
-        time: '1h'
-    },
-    {
-        id:'2',
-        postId:'2',
-        userId:'2',
-        comment:'Great',
-        time: '56min'
-    }
-]
+export const commentsData = [
+  {
+    id: "1",
+    postId: "1",
+    userId: "1",
+    comment: "SHukriya",
+    time: "23min",
+  },
+  {
+    id: "2",
+    postId: "1",
+    userId: "2",
+    comment: "Great",
+    time: "1h",
+  },
+  {
+    id: "3",
+    postId: "2",
+    userId: "2",
+    comment: "Great",
+    time: "56min",
+  },
+];
--- src/Pages/Comments/Comments.js
+++ src/Pages/Comments/Comments.js
@@ -1,21 +1,41 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import Icon from "@mdi/react";
-import { mdiChevronLeft,mdiCheckDecagram } from "@mdi/js";
-import './comments.css'
+import { mdiChevronLeft, mdiCheckDecagram } from "@mdi/js";
+import "./comments.css";
 import Avatar from "@mui/material/Avatar";
-import {userData} from '../../Data/user'
-import {commentsData} from '../../Data/comments'
+import { userData } from "../../Data/user";
+import { commentsData } from "../../Data/comments";
 import { myProfile } from "../../Data/profile";
-
+import moment from "moment";
 
 function Comments() {
   const { postId } = useParams();
-  const [postComments,setPostComments] = useState();
-  const[comment,setComment] = useState();
+  const [postComments, setPostComments] = useState();
+  const [comment, setComment] = useState();
+
   useEffect(() => {
-    setPostComments(commentsData.filter((item)=>item.postId===postId));
-},[postId])
+    setPostComments(commentsData.filter((item) => item.postId === postId));
+  }, [postId]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newCommentId = Math.max(commentsData.map((item) => Number(item.id)));
+    const obj = {
+      id: newCommentId,
+      postId,
+      userId: myProfile.id,
+      comment,
+      time: moment().format("Do MMMM YY, hh:mm A"),
+    };
+    setComment("");
+    setPostComments([...postComments, obj]);
+  };
+
+  const handleChange = (e) => {
+    setComment(e.target.value);
+  };
 
   return (
     <div>
@@ -23,22 +43,38 @@ function Comments() {
         <Icon className="back_icon" path={mdiChevronLeft} size={1.3} />
       </Link>
       <h3 className="comment_heading">Comments</h3>
-      { postComments &&
-        postComments.map((item)=> 
-        <div className="comment">
-          <Avatar src={userData.find((user)=>user.id===item.userId).profile_picture} alt="abc"/>
-        <div>
-        <strong>{userData.find((user)=>user.id===item.userId).username}{userData.find((user)=>user.id===item.userId).isVerified === "true" && (
-              <Icon path={mdiCheckDecagram} size={0.6} color="#0080ff" />
-            )}</strong>
-        <span>{item.comment}</span>
-        <p>{item.time}</p>
-        </div>
-      </div>)
-      }
-      <form className="comment_post">
-        <Avatar className="post_avatar" src={myProfile.profile_picture}/>
-        <input placeholder="Add a comment" value={comment} onChange={(e)=>setComment(e.target.value)}/>
+      {postComments &&
+        postComments.map((item) => (
+          <div className="comment">
+            <Avatar
+              src={
+                userData.find((user) => user.id === item.userId).profile_picture
+              }
+              alt="abc"
+            />
+            <div>
+              <strong>
+                {userData.find((user) => user.id === item.userId).username}
+                {userData.find((user) => user.id === item.userId).isVerified ===
+                  "true" && (
+                  <Icon path={mdiCheckDecagram} size={0.6} color="#0080ff" />
+                )}
+              </strong>
+              <span>{item.comment}</span>
+              <p>{item.time}</p>
+            </div>
+          </div>
+        ))}
+      <form className="comment_post" onSubmit={handleSubmit}>
+        <Avatar className="post_avatar" src={myProfile.profile_picture} />
+        <input
+          placeholder="Add a comment"
+          value={comment}
+          onChange={handleChange}
+        />
+        <button type="submit" className="post_button">
+          Post
+        </button>
       </form>
     </div>
   );
