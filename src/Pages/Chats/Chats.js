265323deede2206ee4018fadb55fd693933888c7
Taha <tahamushtaq1122@gmail.com>
2022-12-08 20:38:24 +0500

    chat profile added

--- src/Pages/Chats/Chats.js
+++ src/Pages/Chats/Chats.js
@@ -1,10 +1,11 @@
 import { mdiChevronLeft } from "@mdi/js";
 import Icon from "@mdi/react";
+import { Avatar } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
 import { myProfile } from "../../Data/profile";
+import { userData } from "../../Data/user";
 import "./chats.css";
-
 
 function Chats() {
   return (
@@ -18,7 +19,12 @@ function Chats() {
       <form>
         <input placeholder="Search" />
       </form>
-
+      {userData.map((user) => (
+        <span className="chats">
+          <Avatar src={user.profile_picture} alt="profilepic" />
+          <p>{user.username}</p>
+        </span>
+      ))}
     </div>
   );
 }
