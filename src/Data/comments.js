4e8b64d8687215c7cb3aab2697d973881e131254
Taha <tahamushtaq1122@gmail.com>
2022-12-08 18:09:49 +0500

    comments added

---
+++ src/Data/comments.js
@@ -0,0 +1,14 @@
+export const comment=[
+    {
+        id:'1',
+        postId:'1',
+        userId:'1',
+        comments:'SHukriya'
+    },
+    {
+        id:'2',
+        postId:'1',
+        userId:'2',
+        comments:'Great'
+    }
+]
